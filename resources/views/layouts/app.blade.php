a23e0ead7e17ccdc8ef5be8bbaf23cf765d09e61
Mark Wendelson R. Olog <markwendelson@gmail.com>
2020-05-19 15:25:00 +0800

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -104,7 +104,7 @@
 
     <!-- JS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
     <script type="text/javascript">
         $(window).load(function() {
             $(".page-loader").fadeOut("slow");;
